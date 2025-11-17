203c5d695377440312832853e38685200a21cb8d
upahar windows <upaharlol@gmail.com>
2025-11-17 20:59:01 +0545

    Added FirebaseAuthStream

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_app/screens/screens.dart';
 import 'package:travel_app/themes/theme_provider.dart';
@@ -21,7 +22,22 @@ class TravelApp extends StatelessWidget {
       darkTheme: darkTheme,
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      home: const SafeArea(top: false, child: LoginPage()),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            );
+          } else if (snapshot.hasData) {
+            return const HomeScreen();
+          } else {
+            return const LoginPage();
+          }
+        },
+      ),
     );
   }
 }
