e490713f4ccbdf03298c250bd54e7f015368e812
upahar windows <upaharlol@gmail.com>
2025-11-20 11:41:08 +0545

    Added dismissable container on tap

--- lib/components/explore_components/explore_map.dart
+++ lib/components/explore_components/explore_map.dart
@@ -42,249 +42,146 @@ class _ExploreMapWidgetState extends State<ExploreMapWidget> {
   }
 
   Future<void> _openMap() async {
-  final CollectionReference<Map<String, dynamic>> placesCollection =
-      FirebaseFirestore.instance.collection('CollectionOfPlaces');
+    final CollectionReference<Map<String, dynamic>> placesCollection =
+        FirebaseFirestore.instance.collection('CollectionOfPlaces');
 
-  if (!mounted) return;
+    if (!mounted) return;
 
-  final ValueNotifier<LatLng?> selectedMarkerNotifier = ValueNotifier<LatLng?>(null);
+    final ValueNotifier<LatLng?> selectedMarkerNotifier =
+        ValueNotifier<LatLng?>(null);
 
-  showModalBottomSheet(
-    useSafeArea: true,
-    isScrollControlled: true,
-    context: context,
-    builder: (BuildContext context) => SafeArea(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.8,
-        child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-          stream: placesCollection.snapshots(),
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
+    showModalBottomSheet(
+      useSafeArea: true,
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext context) => SafeArea(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            stream: placesCollection.snapshots(),
+            builder:
+                (
+                  BuildContext context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot,
+                ) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
 
-            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-              return const Center(
-                child: Icon(Icons.error, size: 80, color: Colors.red),
-              );
-            }
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return const Center(
+                      child: Icon(Icons.error, size: 80, color: Colors.red),
+                    );
+                  }
 
-            final List<Marker> markers = snapshot.data!.docs.map((QueryDocumentSnapshot<Map<String, dynamic>> doc) {
-              final Map<String, dynamic> data = doc.data();
-              final double lat = data['latitude'];
-              final double lng = data['longitude'];
+                  final List<Marker> markers = snapshot.data!.docs.map((
+                    QueryDocumentSnapshot<Map<String, dynamic>> doc,
+                  ) {
+                    final Map<String, dynamic> data = doc.data();
+                    final double lat = data['latitude'];
+                    final double lng = data['longitude'];
 
-              return Marker(
-                point: LatLng(lat, lng),
-                child: GestureDetector(
-                  onTap: () {
-                    selectedMarkerNotifier.value = LatLng(lat, lng);
-                  },
-                  child: const Icon(
-                    Icons.location_pin,
-                    size: 35,
-                    color: Colors.red,
-                  ),
-                ),
-              );
-            }).toList();
-
-            return Stack(
-              children: <Widget>[
-                FlutterMap(
-                  mapController: _mapController,
-                  options: MapOptions(
-                    initialCenter: _currentLocation,
-                    initialZoom: 2,
-                    minZoom: 0,
-                    maxZoom: 19,
-                  ),
-                  children: <Widget>[
-                    TileLayer(
-                      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                      fallbackUrl: 'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}.png',
-                    ),
-                    CurrentLocationLayer(
-                      alignPositionOnUpdate: AlignOnUpdate.always,
-                      alignDirectionOnUpdate: AlignOnUpdate.never,
-                      style: const LocationMarkerStyle(
-                        marker: DefaultLocationMarker(
-                          child: Icon(
-                            Icons.location_pin,
-                            color: Colors.black38,
-                          ),
+                    return Marker(
+                      point: LatLng(lat, lng),
+                      child: GestureDetector(
+                        onTap: () {
+                          selectedMarkerNotifier.value = LatLng(lat, lng);
+                        },
+                        child: const Icon(
+                          Icons.location_pin,
+                          size: 35,
+                          color: Colors.red,
                         ),
-                        markerSize: Size(30, 30),
-                        markerDirection: MarkerDirection.heading,
-                      ),
-                    ),
-                    MarkerLayer(markers: markers),
-                  ],
-                ),
-
-                Positioned(
-                  bottom: 20,
-                  right: 20,
-                  child: FloatingActionButton(
-                    onPressed: () {
-                      _mapController.move(_currentLocation, 2);
-                    },
-                    elevation: 5,
-                    child: const Icon(Icons.my_location),
-                  ),
-                ),
-
-                ValueListenableBuilder<LatLng?>(
-                  valueListenable: selectedMarkerNotifier,
-                  builder: (BuildContext context, LatLng? selectedMarker, Widget? child) {
-                    if (selectedMarker == null) return const SizedBox.shrink(); // Invisible when null.
-
-                    return Positioned(
-                      top: 50,
-                      left: 50,
-                      child: Container(
-                        width: 150,
-                        height: 80,
-                        color: Colors.white,
-                        child: const Center(child: Text('Marker Info')),
                       ),
                     );
-                  },
-                ),
-              ],
-            );
-          },
+                  }).toList();
+
+                  return Stack(
+                    children: <Widget>[
+                      FlutterMap(
+                        mapController: _mapController,
+                        options: MapOptions(
+                          initialCenter: _currentLocation,
+                          initialZoom: 2,
+                          minZoom: 0,
+                          maxZoom: 19,
+                          onTap: (TapPosition tapPosition, LatLng latLng) {
+                            selectedMarkerNotifier.value = null;
+                          }
+                        ),
+                        children: <Widget>[
+                          TileLayer(
+                            urlTemplate:
+                                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                            fallbackUrl:
+                                'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}.png',
+                          ),
+                          CurrentLocationLayer(
+                            alignPositionOnUpdate: AlignOnUpdate.always,
+                            alignDirectionOnUpdate: AlignOnUpdate.never,
+                            style: const LocationMarkerStyle(
+                              marker: DefaultLocationMarker(
+                                child: Icon(
+                                  Icons.location_pin,
+                                  color: Colors.black38,
+                                ),
+                              ),
+                              markerSize: Size(30, 30),
+                              markerDirection: MarkerDirection.heading,
+                            ),
+                          ),
+                          MarkerLayer(markers: markers),
+                        ],
+                      ),
+                  
+                      Positioned(
+                        bottom: 20,
+                        right: 20,
+                        child: FloatingActionButton(
+                          onPressed: () {
+                            _mapController.move(_currentLocation, 2);
+                          },
+                          elevation: 5,
+                          child: const Icon(Icons.my_location),
+                        ),
+                      ),
+                  
+                  // The container that is displayed when tapped on the marker
+                      ValueListenableBuilder<LatLng?>(
+                        valueListenable: selectedMarkerNotifier,
+                        builder:
+                            (
+                              BuildContext context,
+                              LatLng? selectedMarker,
+                              Widget? child,
+                            ) {
+                              if (selectedMarker == null) {
+                                return const SizedBox.shrink();
+                              }
+                              return Positioned(
+                                top: 50,
+                                left: 50,
+                                child: Container(
+                                  width: 150,
+                                  height: 80,
+                                  color: Colors.white,
+                                  child: const Center(
+                                    child: Text('Marker Info'),
+                                  ),
+                                ),
+                              );
+                            },
+                      ),
+                    ],
+                  );
+                },
+          ),
         ),
       ),
-    ),
-  ).whenComplete(() {
-    selectedMarkerNotifier.dispose();
-  });
-}
-
-  // Future<void> _openMap() async {
-  //   final CollectionReference<Map<String, dynamic>> placesCollection =
-  //       FirebaseFirestore.instance.collection('CollectionOfPlaces');
-
-  //   if (!mounted) return;
-
-  //   showModalBottomSheet(
-  //     useSafeArea: true,
-  //     isScrollControlled: true,
-  //     context: context,
-  //     builder: (BuildContext context) => SafeArea(
-  //       child: SizedBox(
-  //         height: MediaQuery.of(context).size.height * 0.8,
-  //         child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-  //           stream: placesCollection.snapshots(),
-  //           builder:
-  //               (
-  //                 BuildContext context,
-  //                 AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot,
-  //               ) {
-  //                 if (snapshot.connectionState == ConnectionState.waiting) {
-  //                   return const Center(child: CircularProgressIndicator());
-  //                 }
-
-  //                 if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-  //                   return const Center(
-  //                     child: Icon(Icons.error, size: 80, color: Colors.red),
-  //                   );
-  //                 }
-
-  //                 final List<Marker> markers = snapshot.data!.docs.map((
-  //                   QueryDocumentSnapshot<Map<String, dynamic>> doc,
-  //                 ) {
-  //                   final Map<String, dynamic> data = doc.data();
-  //                   final double lat = data['latitude'];
-  //                   final double lng = data['longitude'];
-  //                   // final String title = data['title'];
-
-  //                   return Marker(
-  //                     point: LatLng(lat, lng),
-  //                     child: GestureDetector(
-  //                       onTap: () {
-  //                         setState(() {
-  //                           _selectedMarker = LatLng(lat, lng);
-  //                         });
-  //                       },
-  //                       child: const Icon(
-  //                         Icons.location_pin,
-  //                         size: 35,
-  //                         color: Colors.red,
-  //                       ),
-  //                     ),
-  //                   );
-  //                 }).toList();
-
-  //                 return Stack(
-  //                   children: <Widget>[
-  //                     FlutterMap(
-  //                       mapController: _mapController,
-  //                       options: MapOptions(
-  //                         initialCenter: _currentLocation,
-  //                         initialZoom: 2,
-  //                         minZoom: 0,
-  //                         maxZoom: 19,
-  //                       ),
-  //                       children: <Widget>[
-  //                         TileLayer(
-  //                           urlTemplate:
-  //                               'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-  //                           fallbackUrl:
-  //                               'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}.png',
-  //                           // userAgentPackageName: 'com.example.app',
-  //                         ),
-  //                         CurrentLocationLayer(
-  //                           alignPositionOnUpdate: AlignOnUpdate.always,
-  //                           alignDirectionOnUpdate: AlignOnUpdate.never,
-  //                           style: const LocationMarkerStyle(
-  //                             marker: DefaultLocationMarker(
-  //                               child: Icon(
-  //                                 Icons.location_pin,
-  //                                 color: Colors.black38,
-  //                               ),
-  //                             ),
-  //                             markerSize: Size(30, 30),
-  //                             markerDirection: MarkerDirection.heading,
-  //                           ),
-  //                           // moveAnimationDuration: Duration.zero,
-  //                         ),
-  //                         MarkerLayer(markers: markers),
-  //                       ],
-  //                     ),
-
-  //                     Positioned(
-  //                       bottom: 20,
-  //                       right: 20,
-  //                       child: FloatingActionButton(
-  //                         onPressed: () {
-  //                           _mapController.move(_currentLocation, 2);
-  //                         },
-  //                         elevation: 5,
-  //                         child: const Icon(Icons.my_location),
-  //                       ),
-  //                     ),
-
-  //                     if (_selectedMarker != null)
-  //                       Positioned(
-  //                         top: 50, // 50 pixels from top of Stack
-  //                         left: 50, // 50 pixels from left of Stack
-  //                         child: Container(
-  //                           width: 150,
-  //                           height: 80, // give it some height
-  //                           color: Colors.white,
-  //                           child: const Center(child: Text("Marker Info")),
-  //                         ),
-  //                       ),
-  //                   ],
-  //                 );
-  //               },
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
+    ).whenComplete(() {
+      selectedMarkerNotifier.dispose();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
